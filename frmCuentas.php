0ea203a5ef3bc0048040f410855b9918f313ca6a
Cristhoper Vargas <112445661+crisss35@users.noreply.github.com>
2025-09-01 18:06:25 -0400

    Mantenedor cuentas

--- frmCuentas.php
+++ frmCuentas.php
@@ -143,16 +143,22 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="shortcut icon" href="images/MC.ico" type="favicon/ico" />
-		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
 		<script src="js/jquery.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
+		<!-- <script src="js/bootstrap.min.js"></script> -->
+
+		<script src="https://cdn.tailwindcss.com"></script>
+		<script src="js/tailwind.js"></script>
 
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Saira&display=swap" rel="stylesheet">
 
+		<link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
+
 		<link rel="stylesheet" type="text/css" href="css/StConta.css">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+		<script src="https://kit.fontawesome.com/b8e5063394.js" crossorigin="anonymous"></script>
 
 		<script type="text/javascript">
 			function CtaCont(){
@@ -174,10 +180,13 @@
 				url: url,
 				data: $('#form1').serialize(),
 				success:function(resp){
-					$('#TableCta').html(resp);
+					// $('#TableCta').html(resp);
+					$("#contenidoTablaModal").html(resp);
 				}
 				});				
 			}
+
+
 
 			function Baja(valor){
 				form1.idempb.value=valor;
@@ -212,6 +221,18 @@
 				form1.target="";
 				form1.action="#";
 			}
+
+			function limpiarFormulario() {
+				document.getElementById("SelCat").value = "";
+				document.getElementById("numero").value = "";
+				document.getElementById("nombre").value = "";
+				document.getElementById("ingreso_no").checked = true;
+				document.getElementById("aux_n").checked = true;
+				
+				// Reset form action
+				form1.action = "xfrmCuentas.php";
+				form1.idmod.value = "";
+			}
 				
 
 		</script>
@@ -222,148 +243,194 @@
 		include 'nav.php';
 	?>
 
-		<div class="container-fluid">
-		<div class="row content">
-			
-			<form action="xfrmCuentas.php" method="POST" name="form1" id="form1">
-				<input type="hidden" name="idempb" id="idempb">
-				<input type="hidden" name="idempa" id="idempa">
-				<input type="hidden" name="ideli" id="ideli">
-				<input type="hidden" name="idmod" id="idmod" value="<?php echo $_POST['idmod'];?>">
+		<div class="min-h-screen bg-gray-50">
+			<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+				<div class="space-y-8">
 
+				<form action="xfrmCuentas.php" method="POST" name="form1" id="form1">
+					<!-- Hidden inputs -->
+					<input type="hidden" name="idempb" id="idempb">
+					<input type="hidden" name="idempa" id="idempa">
+					<input type="hidden" name="ideli" id="ideli">
+					<input type="hidden" name="idmod" id="idmod" value="<?php echo $_POST['idmod'];?>">
 
-				<br>
-				<div class="col-md-8">
-					<div class="panel panel-default" style="background-color: hsl(0, 0%, 0%, 0);">
-						<div class="panel-heading">Mantenedor de Cuentas</div>
-						<div class="panel-body">
+					<!-- Action Buttons -->
+					<div class="flex flex-wrap justify-start items-center gap-2 border-2 border-gray-300 rounded-md p-2 mb-6">
+						<button type="button" 
+								class="bg-slate-100 text-sm hover:bg-gray-300 text-blue-600 font-medium py-1 px-2 border-2 border-blue-600 rounded-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2" 
+								onclick="limpiarFormulario()">
+							<i class="fa fa-plus mr-2"></i>Nueva
+						</button>
+						<?php 
+							if ($sw==1) {
+								echo '<button type="submit" class="bg-gray-100 hover:bg-gray-300 text-sm text-black font-medium py-1 px-2 border-2 border-gray-600 rounded-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2">
+										<i class="fa fa-edit mr-2"></i>Modificar
+									</button>';
+							}else{
+								echo '<button type="submit" class="bg-gray-100 hover:bg-gray-300 text-sm text-black font-medium py-1 px-2 border-2 border-gray-600 rounded-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2">
+										<i class="fa fa-save mr-2"></i>Grabar
+									</button>';
+							}
+						?>
 
-							<div class="col-md-4">
-							<div class="input-group">
-								<span class="input-group-addon">Categor&iacute;a</span>
-								<select class="form-control" id="SelCat" name="SelCat" onchange="CtaCont()" required>
-									<option value="">Selecciones</option>
-									<?php 
-										$mysqli=xconectar($_SESSION['UsuariaSV'],descriptSV($_SESSION['PassSV']),$_SESSION['BaseSV']);
-										$SQL="SELECT * FROM CTCategoria WHERE estado<>'X'";
-										$resultados = $mysqli->query($SQL);
-										while ($registro = $resultados->fetch_assoc()) {
-											if ($xidcategoria!="") {
-												if ($xidcategoria==$registro["id"]) {
-													echo "<option value ='".$registro["id"]."' selected>".$registro["nombre"]."</option>";
+						<button data-modal-target="default-modal" data-modal-toggle="default-modal" class="bg-gray-100 hover:bg-gray-300 text-sm text-black font-medium py-1 px-2 border-2 border-gray-600 rounded-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2" type="button">
+						<i class="fa-solid fa-magnifying-glass text-gray-600 mr-2"></i>Buscar
+						</button>
+
+						<button type="button" 
+							class="bg-gray-100 hover:bg-gray-300 text-sm text-black font-medium py-1 px-2 border-2 border-gray-600 rounded-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2" 
+							onclick="GenLibro()">
+							<i class="fa fa-file-excel-o mr-2"></i>Exportar Excel
+						</button>   
+
+						<button type="button" class="bg-gray-100 hover:bg-gray-300 text-sm text-black font-medium py-1 px-2 border-2 border-gray-600 rounded-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
+								onclick="Volver()">
+							<i class="fa fa-times mr-2"></i>Cancelar
+						</button>
+					</div>
+
+					<!-- Main Form Card -->
+					<div class="bg-white rounded-lg shadow-sm border border-gray-200">
+						<div class="flex justify-start items-center px-6 pt-3 pb-3 bg-gray-100 w-full shadow">
+							<div class="w-10 h-10 bg-blue-100 rounded-lg flex justify-center items-center mr-4">
+								<i class="fas fa-list-alt text-lg text-blue-600"></i>
+							</div>
+							<div>
+								<h3 class="text-lg font-semibold text-gray-800">
+									Mantenedor de Cuentas
+								</h3>
+								<p class="text-sm text-gray-600">Gestión de cuentas contables</p>     
+							</div>
+						</div>
+						<div class="p-6 pt-1 space-y-6">
+
+							<!-- Category Selection -->
+							<div class="grid grid-cols-1 md:grid-cols-1 gap-6">
+								<div class="mt-3">
+									<label for="SelCat" class="block text-sm font-medium text-gray-700 mb-1 pl-1">
+										<i class="fa fa-tags mr-1"></i>Categoría
+									</label>
+									<select class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" id="SelCat" name="SelCat" onchange="CtaCont()" required>
+										<option value="">Seleccione</option>
+										<?php 
+											$mysqli=xconectar($_SESSION['UsuariaSV'],descriptSV($_SESSION['PassSV']),$_SESSION['BaseSV']);
+											$SQL="SELECT * FROM CTCategoria WHERE estado<>'X'";
+											$resultados = $mysqli->query($SQL);
+											while ($registro = $resultados->fetch_assoc()) {
+												if ($xidcategoria!="") {
+													if ($xidcategoria==$registro["id"]) {
+														echo "<option value ='".$registro["id"]."' selected>".$registro["nombre"]."</option>";
+													}else{
+														echo "<option value ='".$registro["id"]."'>".$registro["nombre"]."</option>";
+													}
 												}else{
 													echo "<option value ='".$registro["id"]."'>".$registro["nombre"]."</option>";
 												}
-											}else{
-												echo "<option value ='".$registro["id"]."'>".$registro["nombre"]."</option>";
 											}
-										}
-										$mysqli->close();
-									?>
-								</select>
-							</div>
+											$mysqli->close();
+										?>
+									</select>
+								</div>
 							</div>
 
-							<div class="clearfix"></div>
-							<br>
+							<div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+								<div>
+									<label for="numero" class="block text-sm font-medium text-gray-700 mb-1 pl-1">
+										<i class="fa fa-hashtag mr-1"></i>Número
+									</label>
+									<input type="text" 
+										   class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" 
+										   id="numero" 
+										   name="numero" 
+										   autocomplete="off" 
+										   value="<?php echo $xnumero; ?>" 
+										   <?php if($sw==1){ echo 'readonly="false"';}?> 
+										   required>
+								</div>
 
-							<div class="col-md-3">
-							<div class="input-group">
-								<span class="input-group-addon">N&uacute;mero</span>
-								<input type="text" class="form-control" id="numero" name="numero" autocomplete="off" value="<?php echo $xnumero; ?>" <?php if($sw==1){ echo 'readonly="false"';}?> required>
+								<div>
+									<label for="nombre" class="block text-sm font-medium text-gray-700 mb-1 pl-1">
+										<i class="fa fa-font mr-1"></i>Nombre
+									</label>
+									<input type="text" 
+										   class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" 
+										   id="nombre" 
+										   name="nombre" 
+										   onChange="javascript:this.value=this.value.toUpperCase();" 
+										   value="<?php echo $xdetalle; ?>"  
+										   autocomplete="off" 
+										   required>
+								</div>
 							</div>
-							</div> 
-
-							<div class="col-md-9">
-							<div class="input-group">
-								<span class="input-group-addon">Nombre</span>
-								<input type="text" class="form-control" id="nombre" name="nombre" onChange="javascript:this.value=this.value.toUpperCase();" value="<?php echo $xdetalle; ?>"  autocomplete="off" required>
-							</div>
-							</div> 
-
-							<div class="clearfix"> </div>
-							<br>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-md-2">
-					<div class="panel panel-default" style="background-color: hsl(0, 0%, 0%, 0);">
-					<div class="panel-heading">Control de Ingreso o Egreso</div>
-						<div class="panel-body">
-							<div class="radio">
-							<label><input type="radio" name="t1" value="S" <?php if ($sw1==1) { echo "checked"; } ?>>SI</label>
-							</div>
-							<div class="radio">
-							<label><input type="radio" name="t1" value="N" <?php if ($sw1==0) { echo "checked"; } ?>>NO</label>
 							</div>
 						</div>
 					</div>
-				</div>
 
-				<div class="col-md-2">
-					<div class="panel panel-default" style="background-color: hsl(0, 0%, 0%, 0);">
-					<div class="panel-heading">Control Auxiliar</div>
-						<div class="panel-body">
-							<div class="radio">
-							<label><input type="radio" name="opt1" value="X" <?php if ($xauxiliar=="X") { echo "checked"; } ?>>Auxiliar</label>
+					<!-- Control Cards Row -->
+					<div class="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+						<!-- Income/Expense Control Card -->
+						<div class="bg-white rounded-lg shadow-sm border border-gray-200">
+							<div class="flex justify-start items-center px-6 pt-3 pb-3 bg-gray-100 w-full shadow">
+								<div class="w-10 h-10 bg-green-100 rounded-lg flex justify-center items-center mr-4">
+									<i class="fas fa-exchange-alt text-lg text-green-600"></i>
+								</div>
+								<div>
+									<h3 class="text-lg font-semibold text-gray-800">
+										Control de Ingreso o Egreso
+									</h3>
+									<p class="text-sm text-gray-600">Tipo de cuenta</p>     
+								</div>
 							</div>
-							<div class="radio">
-							<label><input type="radio" name="opt1" value="E" <?php if ($xauxiliar=="E") { echo "checked"; } ?>>Efectivo</label>
+							<div class="p-6 pt-1 space-y-4 mt-3">
+								<div class="flex items-center">
+									<input type="radio" id="ingreso_si" name="t1" value="S" <?php if ($sw1==1) { echo "checked"; } ?> class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2">
+									<label for="ingreso_si" class="ml-2 text-sm font-medium text-gray-700">Sí</label>
+								</div>
+								<div class="flex items-center">
+									<input type="radio" id="ingreso_no" name="t1" value="N" <?php if ($sw1==0) { echo "checked"; } ?> class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2">
+									<label for="ingreso_no" class="ml-2 text-sm font-medium text-gray-700">No</label>
+								</div>
 							</div>
-							<div class="radio">
-							<label><input type="radio" name="opt1" value="B" <?php if ($xauxiliar=="B") { echo "checked"; } ?>>Banco</label>
+						</div>
+
+						<!-- Auxiliary Control Card -->
+						<div class="bg-white rounded-lg shadow-sm border border-gray-200">
+							<div class="flex justify-start items-center px-6 pt-3 pb-3 bg-gray-100 w-full shadow">
+								<div class="w-10 h-10 bg-purple-100 rounded-lg flex justify-center items-center mr-4">
+									<i class="fas fa-cogs text-lg text-purple-600"></i>
+								</div>
+								<div>
+									<h3 class="text-lg font-semibold text-gray-800">
+										Control Auxiliar
+									</h3>
+									<p class="text-sm text-gray-600">Tipo de control</p>     
+								</div>
 							</div>
-							<div class="radio">
-							<label><input type="radio" name="opt1" value="N" <?php if ($xauxiliar=="N" || $xauxiliar=="O") { echo "checked"; } ?>>No Aplica</label>
+							<div class="p-6 pt-1 space-y-4 mt-5">
+								<div class="flex items-center">
+									<input type="radio" id="aux_x" name="opt1" value="X" <?php if ($xauxiliar=="X") { echo "checked"; } ?> class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2">
+									<label for="aux_x" class="ml-2 text-sm font-medium text-gray-700">Auxiliar</label>
+								</div>
+								<div class="flex items-center">
+									<input type="radio" id="aux_e" name="opt1" value="E" <?php if ($xauxiliar=="E") { echo "checked"; } ?> class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2">
+									<label for="aux_e" class="ml-2 text-sm font-medium text-gray-700">Efectivo</label>
+								</div>
+								<div class="flex items-center">
+									<input type="radio" id="aux_b" name="opt1" value="B" <?php if ($xauxiliar=="B") { echo "checked"; } ?> class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2">
+									<label for="aux_b" class="ml-2 text-sm font-medium text-gray-700">Banco</label>
+								</div>
+								<div class="flex items-center">
+									<input type="radio" id="aux_n" name="opt1" value="N" <?php if ($xauxiliar=="N" || $xauxiliar=="O") { echo "checked"; } ?> class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2">
+									<label for="aux_n" class="ml-2 text-sm font-medium text-gray-700">No Aplica</label>
+								</div>
 							</div>
 						</div>
 					</div>
+
+
+				</form>
 				</div>
-
-				<div class="clearfix"></div>
-
-				<div class="col-md-8 text-right">
-					<?php 
-						if ($sw==1) {
-					?>
-						<button type="submit" class="btn btn-modificar">
-							<span class="glyphicon glyphicon-edit"></span> Modificar
-						</button>
-
-					<?php 
-						}else{
-					?>
-						<button type="submit" class="btn btn btn-grabar">
-							<span class="glyphicon glyphicon-floppy-saved"></span> Grabar
-						</button>
-
-					<?php 
-						}
-					?>
-						<button type="button" class="btn btn-cancelar" onclick="Volver()">
-							<span class="glyphicon glyphicon-remove"></span> Cancelar
-						</button>      
-				</div>
-
-				<div class="clearfix"></div>
-				<br>
-				<div class="col-md-12">
-					<div cl class="col-sm-10">
-						<input class="form-control" id="myInput" type="text" placeholder="Buscar...">
-					</div>
-					<div cl class="col-sm-2">
-						<button type="button" class="btn btn-sm btn-exportar btn-block" onclick="GenLibro()">
-							<span class="glyphicon glyphicon-file"></span> Exportar Excel
-						</button>      
-					</div>
-					<div class="clearfix"></div>
-					<br>
-					<div id="TableCta">
-					</div>
-				</div>
-			</form>
-		</div>
+			</div>
 		</div>
 		<script>
 			<?php
@@ -377,9 +444,53 @@
 					echo 'alert ("Esta cuenta tiene movimientos y puede estar utilizada en alguna empresa, ya que es plan de cuenta comun, no se puede eliminar.");';                
 				}
 			?>
-		</script>			
+		</script>	
+		
+		<div id="default-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+			<div class="relative p-4 w-full max-w-7xl max-h-full">
+				<!-- Modal content -->
+				<div class="relative bg-white rounded-lg shadow-sm">
+					<!-- Modal header -->
+					<div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-gray-200">
+						<div class="flex items-center">
+							<div class="w-10 h-10 bg-blue-100 rounded-lg flex justify-center items-center mr-4">
+								<i class="fa fa-list text-lg text-primary-500"></i>
+							</div>
+							<div>
+								<h3 class="text-lg font-semibold text-gray-900 flex items-center">
+									Cuentas Creadas
+								</h3>	
+								<p class="text-sm text-gray-600"><?php echo $MsjEmpresa; ?></p>
+							</div>	
+						</div>
+						<button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+							<svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+								<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+							</svg>
+							<span class="sr-only">Close modal</span>
+						</button>
+					</div>
+					<!-- Modal body -->
+					<div class="p-4 md:p-5 space-y-4">
+						<div class="mb-4">
+						<input class="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-blue-500 focus:border-blue-500" 
+							id="myInput" 
+							type="text" 
+							placeholder="Buscar cuentas...">
+						</div>
+						<div class="overflow-x-auto max-h-96">
+							<div id="contenidoTablaModal">
+								<!-- El contenido de la tabla se carga aquí dinámicamente -->
+							</div>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+
 
 		<?php include 'footer.php'; ?>
-
+		<script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
 	</body>
 </html>
--- frmCuentasGrilla.php
+++ frmCuentasGrilla.php
@@ -4,18 +4,18 @@
 	include 'conexion/secciones.php';
 
 echo '
-					<table class="table table-condensed table-hover">
-						<thead>
-							<tr style="background-color: #d9d9d9;">
-								<th width="10%">Codigo</th>
-								<th>Cuenta</th>
-								<th>T&iacute;po</th>
-								<th>Categor&iacute;a</th>
-								<th class="text-center">Ingreso/Egreso</th>
-								<th class="text-center">Auxiliar</th>
-								<th width="1%"></th>
-								<th width="1%"></th>
-								<th width="1%"></th>
+					<table class="min-w-full divide-y divide-gray-200">
+						<thead class="bg-gray-50">
+							<tr>
+								<th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" width="10%">Codigo</th>
+								<th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Cuenta</th>
+								<th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">T&iacute;po</th>
+								<th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Categor&iacute;a</th>
+								<th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Ingreso/Egreso</th>
+								<th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Auxiliar</th>
+								<th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" width="1%"></th>
+								<th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" width="1%"></th>
+								<th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" width="1%"></th>
 							</tr>
 						</thead>
 						<tbody id="myTable">
@@ -54,46 +54,46 @@ echo '
 			// }
 			if ($_SERVER["REQUEST_URI"]=="/frmCuentasXLS.php") {
 				echo '
-					<tr>
-					<td>'.$registro["numero"].'</td>
-					<td>'.strtoupper(utf8_decode($registro["detalle"])).'</td>
-					<td>'.$tiptip.'</td>
-					<td>'.utf8_decode($tipcat).'</td>
+					<tr class="bg-white hover:bg-gray-50 transition duration-150 ease-in-out">
+					<td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">'.$registro["numero"].'</td>
+					<td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">'.mb_strtoupper($registro["detalle"], 'UTF-8').'</td>
+					<td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">'.$tiptip.'</td>
+					<td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">'.mb_strtoupper($tipcat, 'UTF-8').'</td>
 					<td class="text-center">'.$mens.'</td>
 				';
 			}else{
 				echo '
-					<tr>
-					<td>'.$registro["numero"].'</td>
-					<td>'.strtoupper($registro["detalle"]).'</td>
-					<td>'.$tiptip.'</td>
-					<td>'.$tipcat.'</td>
+					<tr class="bg-white hover:bg-gray-50 transition duration-150 ease-in-out">
+					<td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">'.$registro["numero"].'</td>
+					<td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">'.mb_strtoupper($registro["detalle"], 'UTF-8').'</td>
+					<td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">'.$tiptip.'</td>
+					<td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">'.mb_strtoupper($tipcat, 'UTF-8').'</td>
 					<td class="text-center">'.$mens.'</td>
 				';
 			}
 
 				if($registro["auxiliar"]=="E"){
-					echo '<td class="text-center">EFECTIVO</td>';
+					echo '<td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">EFECTIVO</td>';
 				}else{
 					if($registro["auxiliar"]=="B"){
-						echo '<td class="text-center">BANCO</td>';
+						echo '<td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">BANCO</td>';
 					}else{
 						if($registro["auxiliar"]=="X"){
-							echo '<td class="text-center">AUXILIAR</td>';
+							echo '<td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">AUXILIAR</td>';
 						}else{
-								echo '<td></td>';
+								echo '<td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"></td>';
 						}
 					}
 				}
 
-				echo '<td><button type="button" class="btn btn-modificar btn-xs" onclick="Modifi('.$registro["id"].')">Modificar</button></td>';
+				echo '<td><button type="button" class="mr-2 bg-gray-100 hover:bg-gray-300 text-sm text-black font-medium py-1 px-2 border-2 border-gray-600 rounded-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2" onclick="Modifi('.$registro["id"].')">Modificar</button></td>';
 
 			if($registro["estado"]=="B"){
-				echo '<td><button type="button" class="btn btn-success btn-xs" onclick="Alta('.$registro["id"].')">Alta</button></td>';
+				echo '<td><button type="button" class="mr-2 bg-gray-100 hover:bg-gray-300 text-sm text-black font-medium py-1 px-2 border-2 border-gray-600 rounded-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2" onclick="Alta('.$registro["id"].')">Alta</button></td>';
 			}else{
-				echo '<td><button type="button" class="btn btn-warning btn-xs" onclick="Baja('.$registro["id"].')">Baja</button></td>';
+				echo '<td><button type="button" class="mr-2 bg-gray-100 hover:bg-gray-300 text-sm text-black font-medium py-1 px-2 border-2 border-gray-600 rounded-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2" onclick="Baja('.$registro["id"].')">Baja</button></td>';
 			}
-			echo '<td><button type="button" class="btn btn-cancelar btn-xs" onclick="Elimina('.$registro["id"].')">Eliminar</button></td>';
+			echo '<td><button type="button" class="bg-gray-100 hover:bg-gray-300 text-sm text-black font-medium py-1 px-2 border-2 border-gray-600 rounded-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2" onclick="Elimina('.$registro["id"].')">Eliminar</button></td>';
 
 
 			echo '

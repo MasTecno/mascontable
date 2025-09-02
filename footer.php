4e017ee975a11307c0af35d2e90cb333f75d5eaa
Cristhoper Vargas <112445661+crisss35@users.noreply.github.com>
2025-09-02 18:08:37 -0400

    actualizando footer

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
-<footer class="container-fluid text-center" style="background-color: #626c76;">
-  <p> &copy; 2016 MasContable | Dise&ntilde;ado por: <a href="https://www.mastecno.cl" target="_blank" style="color: #fff;">Mastecno.cl</a></p>
+<footer class="text-center" style="background-color: #626c76;">
+  <p> &copy; 2016 MasContable | Dise&ntilde;ado por: <a class="text-white hover:text-red-500" href="https://www.mastecno.cl" target="_blank" style="color: #fff;">Mastecno.cl</a></p>
 </footer>

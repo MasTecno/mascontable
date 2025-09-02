c9bb7d757e3a420912d0f3a9d02baefbfeaa3426
Cristhoper Vargas <112445661+crisss35@users.noreply.github.com>
2025-09-02 10:35:21 -0400

    cambios vista costos

--- CCostos/index.php
+++ CCostos/index.php
@@ -51,16 +51,19 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="shortcut icon" href="../images/MC.ico" type="favicon/ico" />
-		<link rel="stylesheet" href="../css/bootstrap.min.css">
 		<script src="../js/jquery.min.js"></script>
-		<script src="../js/bootstrap.min.js"></script>
+
+		<script src="https://cdn.tailwindcss.com"></script>
+		<script src="../js/tailwind.js"></script>
 
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Saira&display=swap" rel="stylesheet">
 
+		<link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
+
 		<link rel="stylesheet" type="text/css" href="../css/StConta.css">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<script src="https://kit.fontawesome.com/b8e5063394.js" crossorigin="anonymous"></script>
 
 
 	<script type="text/javascript">
@@ -102,6 +105,21 @@
 			form1.action="../frmMain.php";
 			form1.submit();
 		}
+
+		function limpiarFormulario(){
+			document.getElementById("codigo").value = "";
+			document.getElementById("codigo").disabled = false;
+			document.getElementById("codigo").readOnly = false;
+			document.getElementById("nombre").value = "";
+			document.getElementById("idccosto").value = "";
+			document.getElementById("idccostob").value = "";
+			document.getElementById("idccostoa").value = "";
+			document.getElementById("idccostoe").value = "";
+
+			document.getElementById("btnGrabar").className = "bg-gray-100 hover:bg-gray-300 text-sm text-black font-medium py-1 px-2 border-2 border-gray-600 rounded-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2";
+			document.getElementById("btnGrabar").innerHTML = "<i class='fa fa-save mr-2'></i>Grabar";
+		}
+
 	</script>  
 
 	</head>
@@ -111,113 +129,189 @@
 		include '../nav.php';
 	?>
 
-	<div class="container-fluid text-left">
-	<div class="row content">
-		<form action="xfrmCCostos.php" method="POST" name="form1" id="form1">
-			<br>
-			<div class="col-sm-2">
-			</div>
-			<div class="col-sm-8">
-				<div class="panel panel-default" style="background-color: hsl(0, 0%, 0%, 0);">
-					<div class="panel-heading text-center">Centros de Costos</div>
-					<div class="panel-body">
+	<div class="min-h-screen bg-gray-50">
+	<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+		<div class="space-y-8">
 
-						<div class="col-md-4">
-							<div class="input-group">
-								<span class="input-group-addon">Codigo</span>
-								<input type="text" class="form-control" id="codigo" name="codigo" autocomplete="off" onChange="javascript:this.value=this.value.toUpperCase();" placeholder="05-ADMIN-MADRID" value="<?php echo $codigo; ?>" <?php if($sw==1){ echo 'readonly="false"';} ?> required>
+			<form action="xfrmCCostos.php" method="POST" name="form1" id="form1" class="space-y-8">
+				
+				<div class="flex flex-wrap justify-start items-center gap-2 border-2 border-gray-300 rounded-md p-2">
+                    <button type="button" 
+                            class="bg-slate-100 text-sm hover:bg-gray-300 text-blue-600 font-medium py-1 px-2 border-2 border-blue-600 rounded-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2" 
+                            onclick="limpiarFormulario()">
+                        <i class="fa fa-plus mr-2"></i>Nuevo
+                    </button>
+                    <?php 
+                        if ($sw==1) {
+                    ?>
+                    <button id="btnGrabar" type="submit" class="bg-gray-100 hover:bg-gray-300 text-sm text-black font-medium py-1 px-2 border-2 border-gray-600 rounded-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2">
+                        <i class="fa fa-edit mr-2"></i>Modificar
+                    </button>
+                    <?php 
+                    }else{
+                    ?>
+                    <button id="btnGrabar" type="submit" class="bg-gray-100 hover:bg-gray-300 text-sm text-black font-medium py-1 px-2 border-2 border-gray-600 rounded-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2">
+                            <i class="fa fa-save mr-2"></i>Grabar
+                    </button>
+                    <?php 
+                    }
+                    ?>
+
+					<button data-modal-target="default-modal" data-modal-toggle="default-modal" class="bg-gray-100 hover:bg-gray-300 text-sm text-black font-medium py-1 px-2 border-2 border-gray-600 rounded-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2" type="button">
+                        <i class="fa-solid fa-magnifying-glass text-gray-600 mr-2"></i>Buscar
+                    </button>
+
+                    <button type="button" class="bg-gray-100 hover:bg-gray-300 text-sm text-black font-medium py-1 px-2 border-2 border-gray-600 rounded-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2" onclick="Volver()">
+                        <i class="fa fa-times mr-2"></i>Cancelar
+                    </button>
+                </div>
+
+				<div class="bg-white rounded-lg shadow-sm border border-gray-200">
+					<div class="flex justify-start items-center px-6 pt-3 pb-3 bg-gray-100 w-full shadow">
+						<div class="w-10 h-10 bg-blue-100 rounded-lg flex justify-center items-center mr-4">
+                            <i class="fas fa-file-alt text-lg text-blue-600"></i>
+                        </div>
+                        <div>
+                            <h3 class="text-lg font-semibold text-gray-800">
+                                Centro de costos
+                            </h3>
+                            <!-- <p class="text-sm text-gray-600">Datos para ingresar un tipo de documento</p>      -->
+                        </div>
+					</div>
+					<div class="p-6 pt-1 space-y-6">
+						
+						<div class="grid grid-cols-2 gap-6 mt-5">
+							<div>
+								<label for="codigo" class="block text-sm font-medium text-gray-700 mb-1 pl-1">
+									<i class="fa fa-file-text mr-1"></i>Codigo
+								</label>
+								<input type="text" class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" id="codigo" name="codigo" autocomplete="off" onChange="javascript:this.value=this.value.toUpperCase();" placeholder="05-ADMIN-MADRID" value="<?php echo $codigo; ?>" <?php if($sw==1){ echo 'readonly="false"';} ?> required>
 							</div>
-
 							<input type="hidden" name="idccosto" id="idccosto" value="<?php echo $_POST['idccosto']; ?>">
 							<input type="hidden" name="idccostob" id="idccostob">
 							<input type="hidden" name="idccostoa" id="idccostoa">
 							<input type="hidden" name="idccostoe" id="idccostoe">
-						</div> 
+							<div>
+								<label for="codigo" class="block text-sm font-medium text-gray-700 mb-1 pl-1">
+									<i class="fa fa-file-text mr-1"></i>Nombre
+								</label>
+								<input type="text" class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" id="nombre" name="nombre" onChange="javascript:this.value=this.value.toUpperCase();" value="<?php echo $nombre; ?>"  autocomplete="off" required>
+							</div>
+						</div>
 
-						<div class="col-md-8">
-						<div class="input-group">
-							<span class="input-group-addon">Nombre</span>
-							<input type="text" class="form-control" id="nombre" name="nombre" onChange="javascript:this.value=this.value.toUpperCase();" value="<?php echo $nombre; ?>"  autocomplete="off" required>
+							<!-- <div class="col-md-4">
+								<div class="input-group">
+									<span class="input-group-addon">Codigo</span>
+									<input type="text" class="form-control" id="codigo" name="codigo" autocomplete="off" onChange="javascript:this.value=this.value.toUpperCase();" placeholder="05-ADMIN-MADRID" value="<?php echo $codigo; ?>" <?php if($sw==1){ echo 'readonly="false"';} ?> required>
+								</div>
+
+								<input type="hidden" name="idccosto" id="idccosto" value="<?php echo $_POST['idccosto']; ?>">
+								<input type="hidden" name="idccostob" id="idccostob">
+								<input type="hidden" name="idccostoa" id="idccostoa">
+								<input type="hidden" name="idccostoe" id="idccostoe">
+							</div> 
+
+							<div class="col-md-8">
+							<div class="input-group">
+								<span class="input-group-addon">Nombre</span>
+								<input type="text" class="form-control" id="nombre" name="nombre" onChange="javascript:this.value=this.value.toUpperCase();" value="<?php echo $nombre; ?>"  autocomplete="off" required>
+							</div> -->
+					</div>
+				</div>
+
+				
+					
+					
+			</form>
+		</div>
+
+		<div id="default-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+			<div class="relative p-4 w-full max-w-7xl max-h-full">
+				<!-- Modal content -->
+				<div class="relative bg-white rounded-lg shadow-sm">
+					<!-- Modal header -->
+					<div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-gray-200">
+						<div class="flex items-center">
+							<div class="w-10 h-10 bg-blue-100 rounded-lg flex justify-center items-center mr-4">
+								<i class="fa fa-list text-lg text-primary-500"></i>
+							</div>
+							<div>
+								<h3 class="text-lg font-semibold text-gray-900 flex items-center">
+									Costos ingresados
+								</h3>	
+								<p class="text-sm text-gray-600">Lista de costos ingresados</p>
+							</div>	
 						</div>
+						<button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+							<svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+								<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+							</svg>
+							<span class="sr-only">Close modal</span>
+						</button>
+					</div>
+					<!-- Modal body -->
+					<div class="p-4 md:p-5 space-y-4">
+
+						<div class="mb-4">
+							<div class="relative">
+								<div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+									<i class="fa fa-search text-gray-400"></i>
+								</div>
+								<input class="w-full pl-10 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" id="myInput" type="text" placeholder="Buscar...">
+							</div>
 						</div>
+						<div class="overflow-x-auto">
+							<table class="min-w-full divide-y divide-gray-200">
+								<thead class="bg-gray-50">
+									<tr>
+										<th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Codigo</th>
+										<th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nombre</th>
+										<th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" width="1%"></th>
+										<th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" width="1%"></th>
+										<th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" width="1%"></th>
+									</tr>
+								</thead>
+								<tbody id="myTable">
+									<?php 
+										$mysqli=xconectar($_SESSION['UsuariaSV'],descriptSV($_SESSION['PassSV']),$_SESSION['BaseSV']);
+										$SQL="SELECT * FROM CTCCosto WHERE estado<>'X' AND rutempresa='".$_SESSION['RUTEMPRESA']."' ORDER BY nombre";
+
+										$resultados = $mysqli->query($SQL);
+										while ($registro = $resultados->fetch_assoc()) {
+											echo '
+												<tr class="bg-white hover:bg-gray-50 transition duration-150 ease-in-out">
+												<td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">'.$registro["codigo"].'</td>
+												<td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">'.$registro["nombre"].'</td>
+											';
+
+												echo '<td><button type="button" class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-warning-700 bg-warning-100 hover:bg-warning-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-warning-500 transition duration-200" onclick="Modifi('.$registro["id"].')">Modificar</button></td>';
+
+												if($registro["estado"]=="B"){
+													echo '<td><button type="button" class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-success-700 bg-success-100 hover:bg-success-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-success-500 transition duration-200" onclick="Alta('.$registro["id"].')">Alta</button></td>';
+												}else{
+													echo '<td><button type="button" class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-gray-700 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition duration-200" onclick="Baja('.$registro["id"].')">Baja</button></td>';
+												}
+
+												echo '<td><button type="button" class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-danger-700 bg-danger-100 hover:bg-danger-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-danger-500 transition duration-200" onclick="Elimi('.$registro["id"].')">Eliminar</button></td>';
+
+
+											echo '
+												</tr>
+											';
+										}       
+										$mysqli->close();
+									?>
+								</tbody>
+							</table>
+						</div>
+						
 					</div>
 				</div>
 			</div>
-			<div class="clearfix"> </div>
-			<br>
-			<div class="col-sm-2">
-			</div>
+		</div>
 
-			<div class="col-md-8 text-right">
-				<?php if ($sw==1) {	?>
-					<button type="submit" class="btn btn-modificar">
-						<span class="glyphicon glyphicon-edit"></span> Modificar
-					</button>
-				<?php }else{ ?>
-					<button type="submit" class="btn btn btn-grabar">
-						<span class="glyphicon glyphicon-floppy-saved"></span> Grabar
-					</button>
-				<?php } ?>
-					<button type="button" class="btn btn-cancelar" onclick="Volver()">
-						<span class="glyphicon glyphicon-remove"></span> Cancelar
-					</button>
-			</div>
-
-
-
-			<div class="clearfix"> </div>
-			<br>
-			<div class="col-sm-2">
-			</div>
-
-			<div class="col-md-8">
-				<input class="form-control" id="myInput" type="text" placeholder="Buscar...">
-				<br>
-				<table class="table table-condensed table-hover">
-					<thead>
-						<tr style="background-color: #d9d9d9;">
-							<th width="20%">Codigo</th>
-							<th>Nombre</th>
-							<th width="1%"></th>
-							<th width="1%"></th>
-							<th width="1%"></th>
-						</tr>
-					</thead>
-					<tbody id="myTable">
-						<?php 
-							$mysqli=xconectar($_SESSION['UsuariaSV'],descriptSV($_SESSION['PassSV']),$_SESSION['BaseSV']);
-							$SQL="SELECT * FROM CTCCosto WHERE estado<>'X' AND rutempresa='".$_SESSION['RUTEMPRESA']."' ORDER BY nombre";
-
-							$resultados = $mysqli->query($SQL);
-							while ($registro = $resultados->fetch_assoc()) {
-								echo '
-									<tr>
-									<td>'.$registro["codigo"].'</td>
-									<td>'.$registro["nombre"].'</td>
-								';
-
-									echo '<td><button type="button" class="btn btn-modificar btn-xs" onclick="Modifi('.$registro["id"].')">Modificar</button></td>';
-
-									if($registro["estado"]=="B"){
-										echo '<td><button type="button" class="btn btn-warning btn-xs" onclick="Alta('.$registro["id"].')">Alta</button></td>';
-									}else{
-										echo '<td><button type="button" class="btn btn-cancelar btn-xs" onclick="Baja('.$registro["id"].')">Baja</button></td>';
-									}
-
-									echo '<td><button type="button" class="btn btn-cancelar btn-xs" onclick="Elimi('.$registro["id"].')">Eliminar</button></td>';
-
-
-								echo '
-									</tr>
-								';
-							}       
-							$mysqli->close();
-						?>
-					</tbody>
-				</table>
-			</div>
-		</form>
-
+		
+		
 		<?php
 			if(isset($_GET['Err']) && $_GET['Err']==1){
 				echo '<script> alert("Este Codigo ya esta ingresado"); </script>';
@@ -242,7 +336,7 @@
 		});
 	</script>
 	<?php include '../footer.php'; ?>
-
+	<script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
 </body>
 </html>
 

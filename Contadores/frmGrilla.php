197158da6c2701fd803929ecfc479eed67a17b06
Cristhoper Vargas <112445661+crisss35@users.noreply.github.com>
2025-09-02 12:26:50 -0400

    mejoras en diseño y grilla

--- Contadores/frmGrilla.php
+++ Contadores/frmGrilla.php
@@ -4,15 +4,15 @@
 	include '../conexion/secciones.php';
 
 echo '
-    <table class="table table-condensed table-hover">
-        <thead>
-            <tr style="background-color: #d9d9d9;">
-                <th width="10%">Rut</th>
-                <th>Nombre</th>
-                <th>Cargo</th>
-                <th width="1%"></th>
-                <th width="1%"></th>
-                <th width="1%"></th>
+    <table class="min-w-full divide-y divide-gray-200">
+        <thead class="bg-gray-50">
+            <tr>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" width="10%">Rut</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nombre</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Cargo</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" width="1%"></th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" width="1%"></th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" width="1%"></th>
             </tr>
         </thead>
         <tbody id="myTable">
@@ -23,19 +23,27 @@ echo '
             while ($registro = $resultados->fetch_assoc()) {
 
                 echo '
-                    <tr>
-                    <td>'.$registro["Rut"].'</td>
-                    <td>'.strtoupper($registro["Nombre"]).'</td>
-                    <td>'.strtoupper($registro["Cargo"]).'</td>
+                    <tr class="bg-white hover:bg-gray-50 transition duration-150 ease-in-out">
+                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">'.$registro["Rut"].'</td>
+                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">'.strtoupper($registro["Nombre"]).'</td>
+                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">'.strtoupper($registro["Cargo"]).'</td>
                 ';
-                echo '<td><button type="button" class="btn btn-modificar btn-xs" onclick="Modifi('.$registro["Id"].')">Modificar</button></td>';
+                echo '<td><button type="button" class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-warning-700 bg-warning-100 hover:bg-warning-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-warning-500 transition duration-200" onclick="Modifi('.$registro["Id"].')">
+                <i class="fa fa-edit mr-1"></i>Modificar
+                </button></td>';
 
                 if($registro["Estado"]=="B"){
-                    echo '<td><button type="button" class="btn btn-success btn-xs" onclick="Alta('.$registro["Id"].')">Alta</button></td>';
+                    echo '<td><button type="button" class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-success-700 bg-success-100 hover:bg-success-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-success-500 transition duration-200" onclick="Alta('.$registro["Id"].')">
+                    <i class="fa fa-check mr-1"></i>Alta
+                    </button></td>';
                 }else{
-                    echo '<td><button type="button" class="btn btn-warning btn-xs" onclick="Baja('.$registro["Id"].')">Baja</button></td>';
+                    echo '<td><button type="button" class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-gray-700 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition duration-200" onclick="Baja('.$registro["Id"].')">
+                    <i class="fa fa-ban mr-1"></i>Baja
+                    </button></td>';
                 }
-                echo '<td><button type="button" class="btn btn-cancelar btn-xs" onclick="Elimina('.$registro["Id"].')">Eliminar</button></td>';
+                echo '<td><button type="button" class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-gray-700 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition duration-200" onclick="Elimina('.$registro["Id"].')">
+                    <i class="fa fa-trash mr-1"></i>Eliminar
+                    </button></td>';
 
 
                 echo '
--- Contadores/index.php
+++ Contadores/index.php
@@ -38,19 +38,22 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="shortcut icon" href="../images/MC.ico" type="favicon/ico" />
-		<link rel="stylesheet" href="../css/bootstrap.min.css">
 		<script src="../js/jquery.min.js"></script>
-		<script src="../js/bootstrap.min.js"></script>
+
+		<script src="https://cdn.tailwindcss.com"></script>
+		<script src="../js/tailwind.js"></script>
 
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Saira&display=swap" rel="stylesheet">
 
+		<link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
+
 		<script src="../js/jquery.Rut.js" type="text/javascript"></script>
 		<script src="../js/jquery.validate.js" type="text/javascript"></script>	
 
 		<link rel="stylesheet" type="text/css" href="../css/StConta.css">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<script src="https://kit.fontawesome.com/b8e5063394.js" crossorigin="anonymous"></script>
 
 		<script type="text/javascript">
 			function Grilla(){
@@ -60,7 +63,7 @@
 				url: url,
 				data: $('#form1').serialize(),
 				success:function(resp){
-					$('#TableContadores').html(resp);
+					$('#contenidoTablaModal').html(resp);
 				}
 				});				
 			}
@@ -85,7 +88,29 @@
 				form1.ideli.value=valor;
 				form1.action="#";
 				form1.submit();
+				limpiarFormulario();
 			}
+
+			function limpiarFormulario(){
+
+				document.getElementById("rut").value = "";
+				document.getElementById("nombre").value = "";
+				document.getElementById("cargo").value = "";
+				document.getElementById("idempb").value = "";
+				document.getElementById("idempa").value = "";
+				document.getElementById("ideli").value = "";
+				document.getElementById("idmod").value = "";
+			}
+
+			function EliminarContador(){
+				const ideli = document.getElementById("idmod").value;
+				form1.ideli.value=ideli;
+				form1.action="#";
+				form1.submit();
+				limpiarFormulario();
+				window.location.href = "index.php";
+			}
+
 			function Volver(){
 				form1.action="frmMain.php";
 				form1.submit();
@@ -104,94 +129,159 @@
 		include '../nav.php';
 	?>
 
-		<div class="container-fluid">
-		<div class="row content">
+		<div class="min-h-screen bg-gray-50">
+			<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+				<div class="space-y-8">
 			
-			<form action="xfrmIndex.php" method="POST" name="form1" id="form1">
-				<input type="hidden" name="idempb" id="idempb">
-				<input type="hidden" name="idempa" id="idempa">
-				<input type="hidden" name="ideli" id="ideli">
-				<input type="hidden" name="idmod" id="idmod" value="<?php echo $_POST['idmod'];?>">
+					<form action="xfrmIndex.php" method="POST" name="form1" id="form1">
 
-				<br>
-				<div class="col-md-8">
-					<div class="panel panel-default" style="background-color: hsl(0, 0%, 0%, 0);">
-						<div class="panel-heading">Mantenedor de Contadores</div>
-						<div class="panel-body">
+						<div class="flex flex-wrap justify-start items-center gap-2 border-2 border-gray-300 rounded-md p-2">
+							<button type="button" 
+									class="bg-slate-100 text-sm hover:bg-gray-300 text-blue-600 font-medium py-1 px-2 border-2 border-blue-600 rounded-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2" 
+									onclick="limpiarFormulario()">
+								<i class="fa fa-plus mr-2"></i>Nuevo
+							</button>
+							<?php 
+								if ($sw==1) {
+							?>
+									<button type="submit" class="bg-gray-100 hover:bg-gray-300 text-sm text-black font-medium py-1 px-2 border-2 border-gray-600 rounded-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2">
+										<i class="fa fa-edit mr-2"></i>Modificar
+									</button>
+								
+									<?php if ($_SESSION['ROL']=="A") { ?>
+										<button type="button" id="btnEliminar" onclick="EliminarContador()" class="bg-gray-100 hover:bg-gray-300 text-sm text-black font-medium py-1 px-2 border-2 border-gray-600 rounded-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2" tabindex="16">
+											<i class="fa fa-trash mr-2"></i>Eliminar
+										</button>
+									<?php } ?>
 
-                            <div class="col-md-6">
-                            <div class="input-group">
-                                <span class="input-group-addon">Rut</span>
-                                <input type="text" class="form-control" id="rut" name="rut" autocomplete="off" placeholder="Ej: 13520300-5" value="<?php echo $rut; ?>" required>
-                            </div>
-                            </div>
-							<div class="clearfix"> </div>
-							<br>
+								<?php 
+								}else{
+								?>
+									<button type="submit" class="bg-gray-100 hover:bg-gray-300 text-sm text-black font-medium py-1 px-2 border-2 border-gray-600 rounded-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2">
+										<i class="fa fa-save mr-2"></i>Grabar
+									</button>
+								<?php 
+								}
+								?>
 
-							<div class="col-md-6">
-							<div class="input-group">
-								<span class="input-group-addon">Nombre Completo</span>
-								<input type="text" class="form-control" maxlength="100" id="nombre" name="nombre" onChange="javascript:this.value=this.value.toUpperCase();" value="<?php echo $xnombre; ?>"  autocomplete="off" required>
+								<button data-modal-target="default-modal" data-modal-toggle="default-modal" class="bg-gray-100 hover:bg-gray-300 text-sm text-black font-medium py-1 px-2 border-2 border-gray-600 rounded-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2" type="button">
+									<i class="fa-solid fa-magnifying-glass text-gray-600 mr-2"></i>Buscar
+								</button>
+
+								<button type="button" class="bg-gray-100 hover:bg-gray-300 text-sm text-black font-medium py-1 px-2 border-2 border-gray-600 rounded-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2" onclick="Volver()">
+									<i class="fa fa-times mr-2"></i>Cancelar
+								</button>
+						</div>
+					
+						<input type="hidden" name="idempb" id="idempb">
+						<input type="hidden" name="idempa" id="idempa">
+						<input type="hidden" name="ideli" id="ideli">
+						<input type="hidden" name="idmod" id="idmod" value="<?php echo $_POST['idmod'];?>">
+
+						<br>
+						<div class="bg-white rounded-lg shadow-sm border border-gray-200">
+							<div class="flex justify-start items-center px-6 pt-3 pb-3 bg-gray-100 w-full shadow">
+								<div class="w-10 h-10 bg-blue-100 rounded-lg flex justify-center items-center mr-4">
+									<i class="fas fa-file-alt text-lg text-blue-600"></i>
+								</div>
+								<div>
+									<h3 class="text-lg font-semibold text-gray-800">
+										Mantenedor de Contadores
+									</h3>
+									<p class="text-sm text-gray-600">Datos para ingresar un contador</p>     
+								</div>
 							</div>
-							</div> 
+							<div class="p-6 pt-1 space-y-6">
 
-							<div class="col-md-6">
-							<div class="input-group">
-								<span class="input-group-addon">Cargo</span>
-								<input type="text" class="form-control" maxlength="100" id="cargo" name="cargo" autocomplete="off" onChange="javascript:this.value=this.value.toUpperCase();" value="<?php echo $xcargo; ?>"  autocomplete="off">
+								<div class="grid grid-cols-1 gap-6 mt-5">
+									<div>
+										<label for="rut" class="block text-sm font-medium text-gray-700 mb-1 pl-1">
+											<i class="fa fa-id-card mr-1"></i>Rut
+										</label>
+										<input type="text" class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" id="rut" name="rut" autocomplete="off" placeholder="Ej: 13520300-5" value="<?php echo $rut; ?>" required>
+									</div>
+								</div>
+
+								<div class="grid grid-cols-2 gap-6 mt-5">
+									<div>
+										<label for="rut" class="block text-sm font-medium text-gray-700 mb-1 pl-1">
+											<i class="fa fa-user mr-1"></i>Nombre Completo
+										</label>
+										<input type="text" class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" maxlength="100" id="nombre" name="nombre" onChange="javascript:this.value=this.value.toUpperCase();" value="<?php echo $xnombre; ?>"  autocomplete="off" required>
+									</div>
+
+									<div>
+										<label for="cargo" class="block text-sm font-medium text-gray-700 mb-1 pl-1">
+											<i class="fa fa-user-tie mr-1"></i>Cargo
+										</label>
+										<input type="text" class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" maxlength="100" id="cargo" name="cargo" autocomplete="off" onChange="javascript:this.value=this.value.toUpperCase();" value="<?php echo $xcargo; ?>"  autocomplete="off">
+									</div>
+								</div>
 							</div>
-							</div>                             
+						</div>
 
-							<div class="clearfix"> </div>
+						
+						<!-- <div class="col-md-12">
+							<div cl class="col-sm-10">
+								<input class="form-control" id="myInput" type="text" placeholder="Buscar...">
+							</div>
+
+							<div class="clearfix"></div>
 							<br>
+							<div id="TableContadores">
+							</div>
+						</div> -->
+					</form>
+				</div>
+			</div>
+		</div>
+
+		<div id="default-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+			<div class="relative p-4 w-full max-w-7xl max-h-full">
+				<!-- Modal content -->
+				<div class="relative bg-white rounded-lg shadow-sm">
+					<!-- Modal header -->
+					<div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-gray-200">
+						<div class="flex items-center">
+							<div class="w-10 h-10 bg-blue-100 rounded-lg flex justify-center items-center mr-4">
+								<i class="fa fa-list text-lg text-primary-500"></i>
+							</div>
+							<div>
+								<h3 class="text-lg font-semibold text-gray-900 flex items-center">
+									Contadores Ingresados
+								</h3>	
+								<!-- <p class="text-sm text-gray-600">Lista de contadores ingresados</p> -->
+							</div>	
+						</div>
+						<button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+							<svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+								<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+							</svg>
+							<span class="sr-only">Close modal</span>
+						</button>
+					</div>
+
+					<div class="p-4 md:p-5 space-y-4">
+						<div class="mb-4">
+						<input class="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-blue-500 focus:border-blue-500" 
+							id="myInput" 
+							type="text" 
+							placeholder="Buscar cuentas...">
+						</div>
+						<div class="overflow-x-auto max-h-96">
+							<div id="contenidoTablaModal">
+								<!-- El contenido de la tabla se carga aquí dinámicamente -->
+							</div>
 						</div>
 					</div>
 				</div>
-
-				<div class="clearfix"></div>
-
-				<div class="col-md-8 text-right">
-					<?php 
-						if ($sw==1) {
-					?>
-						<button type="submit" class="btn btn-modificar">
-							<span class="glyphicon glyphicon-edit"></span> Modificar
-						</button>
-
-					<?php 
-						}else{
-					?>
-						<button type="submit" class="btn btn btn-grabar">
-							<span class="glyphicon glyphicon-floppy-saved"></span> Grabar
-						</button>
-
-					<?php 
-						}
-					?>
-						<button type="button" class="btn btn-cancelar" onclick="Volver()">
-							<span class="glyphicon glyphicon-remove"></span> Cancelar
-						</button>      
-				</div>
-
-				<div class="clearfix"></div>
-				<br>
-				<div class="col-md-12">
-					<div cl class="col-sm-10">
-						<input class="form-control" id="myInput" type="text" placeholder="Buscar...">
-					</div>
-
-					<div class="clearfix"></div>
-					<br>
-					<div id="TableContadores">
-					</div>
-				</div>
-			</form>
+			</div>
 		</div>
-		</div>
+
 		<script>
 			<?php
 				if (isset($_GET['ex']) && $_GET['ex']=="yes") {
-					echo 'alert ("Contador ya registrada");';
+					echo 'alert ("Contador ya registrado");';
 				}
 				if ($NoElimina=="N") {
 					echo 'alert ("Esta cuenta tiene movimientos, no se puede eliminar.");';
@@ -203,6 +293,6 @@
 		</script>			
 
 		<?php include 'footer.php'; ?>
-
+		<script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
 	</body>
 </html>
